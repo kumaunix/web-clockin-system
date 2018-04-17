6710b7c6be1a227831a7a202a8556752d3d2b8ae
kumaunix <tobias.sopu@gmail.com>
2018-04-17 16:44:04 +0900

    Update black_ip.php

--- black_ip.php
+++ black_ip.php
@@ -1,13 +1,15 @@
  $myip = $_SERVER['REMOTE_ADDR'];
  
-//Office 2 IP address Restriction  
+//Office 2 IP address Restriction  and update ip address to restrick clock to this ip address
+// since ip address may change you may put 111.11.00.0 ~ 11.12.00.0 
+
 $add_ip_start ='00.00.00.00'; 
 $add_ip_end = '00.00.00.00';
 $add_ip = ($myip >= $kumamoto_ip_start && $myip <= $kumamoto_ip_end);
 
 //Office 1 IP address Restriction
 $add1_ip_start ='00.00.00.00'; 
-$add1_ip_stop = '117.0.252.50';
+$add1_ip_stop = '00.00.00.00';
 $add1_ip = ($myip >= $hanoi_ip_start && $myip <= $hanoi_ip_stop);
 
 
