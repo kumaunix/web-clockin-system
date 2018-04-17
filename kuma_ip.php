10c86e5f10a470cdf3bcac8b905e0faace109c01
kumaunix <tobias.sopu@gmail.com>
2018-04-17 14:48:21 +0900

    Delete kuma_ip.php

--- kuma_ip.php
+++
@@ -1,5 +0,0 @@
-<?
-//Kumamoto IP address Restriction  
-$kumamoto_ip_start ='222.10.170.1'; 
-$kumamoto_ip_stop = '222.10.170.99';
-$kumamoto_ip = ($_SERVER['REMOTE_ADDR'] >= $kumamoto_ip_start && $_SERVER['REMOTE_ADDR'] <= $kumamoto_ip_stop);
