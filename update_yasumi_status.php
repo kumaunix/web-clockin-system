a1df8e0f22f7f17976e40e04f286ba5e7b738d84
Kiyoshi <tobias.sopu@gmail.com>
2018-09-10 12:04:50 +0900

    Update update_yasumi_status.php

--- update_yasumi_status.php
+++ update_yasumi_status.php
@@ -1,11 +1,11 @@
 <?
-/*   All sick, vacation and leave workers today and updated @8:00 pm. 
+/*   All sick, vacation and leave workers today and updated @5:00 pm. 
  *   Status are updated. They can clock in tomorrow. 
  */ 
 $time = date_default_timezone_get();
 $time_format = date('H:i:s', strtotime($time));
 
-if ($time_format > "19:30:00"){
+if ($time_format > "17:00:00"){
 	$sql=("UPDATE users SET status_today='' ");
     $query = mysqli_query($conn, $sql) or die ("Error Clocking In Into timesheet:".mysqli_error($conn));
 }
